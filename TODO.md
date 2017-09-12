2a1a3a25c256ae747253971ece1a494dba52c839
Daniel Lewis <daniellewis777@gmail.com>
2017-09-12 18:22:53 +0100

    Reorganised tests

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
 * Skip tests if unsupported command
 
 * Write benchmarks for shelljs unsupported commands
+
+* Default options
--- test/benchmarks/benchmarks.spec.js
+++ test/benchmarks/benchmarks.spec.js
@@ -10,89 +10,126 @@ describe('benchmarks: shell-o v shelljs', function() {
   var cmdObj, start, end
   var shelljsCmdTime, shelljsExecTime, shelloTime
 
-  // it('echo - a shelljs supported command', function() {
-  //
-  //   start = now()
-  //   cmdObj = shelljs.exec('echo hello', {silent: true})
-  //   end = now()
-  //   imp.expect(cmdObj.stdout).to.equal('hello' + imp.EOL)
-  //   shelljsExecTime = end - start
-  //   console.log('shelljs.exec time -----> ', shelljsExecTime.toFixed(2))
-  //
-  //   start = now()
-  //   cmdObj = shello('echo hello', {stdio: 'pipe', encoding: 'utf-8'})
-  //   end = now()
-  //   imp.expect(cmdObj.stdout).to.equal('hello' + imp.EOL)
-  //   shelloTime = end - start
-  //   console.log('shello time -----------> ', shelloTime.toFixed(2))
-  //
-  //   start = now()
-  //   cmdObj = shelljs.echo('hello')
-  //   end = now()
-  //   imp.expect(cmdObj.stdout).to.equal('hello')
-  //   shelljsCmdTime = end - start
-  //   console.log('shelljs.echo time -----> ', shelljsCmdTime.toFixed(2))
-  //
-  //   imp.expect(shelljsExecTime).to.be.above(shelloTime * 10)
-  //   imp.expect(shelloTime).to.be.above(shelljsCmdTime * 4)
-  // })
+  describe('shelljs supported commands', function() {
 
-  // it('which - a shelljs supported command', function() {
-  //
-  //   start = now()
-  //   cmdObj = shelljs.exec('which echo', {silent: true})
-  //   end = now()
-  //   imp.expect(cmdObj.code).to.equal(0)
-  //   shelljsExecTime = end - start
-  //   console.log('shelljs.exec time -----> ', shelljsExecTime.toFixed(2))
-  //
-  //   start = now()
-  //   cmdObj = shello('which echo', {stdio: 'pipe', encoding: 'utf-8'})
-  //   end = now()
-  //   imp.expect(cmdObj.code).to.equal(0)
-  //   shelloTime = end - start
-  //   console.log('shello time -----------> ', shelloTime.toFixed(2))
-  //
-  //   start = now()
-  //   cmdObj = shelljs.which('echo')
-  //   end = now()
-  //   imp.expect(cmdObj).to.not.be.null
-  //   imp.expect(cmdObj.code).to.equal(0)
-  //   shelljsCmdTime = end - start
-  //   console.log('shelljs.which time ----> ', shelljsCmdTime.toFixed(2))
-  //
-  //   imp.expect(shelljsExecTime).to.be.above(shelloTime * 1.9)
-  //   imp.expect(shelloTime).to.be.above(shelljsCmdTime * 1.2)
-  // })
+    describe('echo', function() {
 
-  it('cat - a shelljs supported command', function() {
+      before(function() {
+        var echo1 = shello('where echo', {stdio: 'pipe', encoding: 'utf-8'})
+        var echo2 = shello('which echo', {stdio: 'pipe', encoding: 'utf-8'})
+        if (!echo1.ok && !echo2.ok) {
+          this.skip()
+        }
+      })
 
-    var file1 = __dirname + '/file1'
-    var file2 = __dirname + '/file2'
+      it('echo', function() {
 
-    start = now()
-    cmdObj = shelljs.exec('cat ' + file1 + ' ' + file2, {silent: true})
-    end = now()
-    imp.expect(cmdObj.stdout).to.equal('hello\nworld\n')
-    shelljsExecTime = end - start
-    console.log('shelljs.exec time -----> ', shelljsExecTime.toFixed(2))
+        start = now()
+        cmdObj = shelljs.exec('echo hello', {silent: true})
+        end = now()
+        imp.expect(cmdObj.stdout).to.equal('hello' + imp.EOL)
+        shelljsExecTime = end - start
+        console.log('shelljs.exec time -----> ', shelljsExecTime.toFixed(2))
 
-    start = now()
-    cmdObj = shello('cat ' + file1 + ' ' + file2, {stdio: 'pipe', encoding: 'utf-8'})
-    end = now()
-    imp.expect(cmdObj.stdout).to.equal('hello\nworld\n')
-    shelloTime = end - start
-    console.log('shello time -----------> ', shelloTime.toFixed(2))
+        start = now()
+        cmdObj = shello('echo hello', {stdio: 'pipe', encoding: 'utf-8'})
+        end = now()
+        imp.expect(cmdObj.stdout).to.equal('hello' + imp.EOL)
+        shelloTime = end - start
+        console.log('shello time -----------> ', shelloTime.toFixed(2))
 
-    start = now()
-    cmdObj = shelljs.cat(file1, file2)
-    end = now()
-    imp.expect(cmdObj.stdout).to.equal('hello\nworld\n')
-    shelljsCmdTime = end - start
-    console.log('shelljs.cat time ------> ', shelljsCmdTime.toFixed(2))
+        start = now()
+        cmdObj = shelljs.echo('hello')
+        end = now()
+        imp.expect(cmdObj.stdout).to.equal('hello')
+        shelljsCmdTime = end - start
+        console.log('shelljs.echo time -----> ', shelljsCmdTime.toFixed(2))
 
-    imp.expect(shelljsExecTime).to.be.above(shelloTime * 3.5)
-    // shello sometimes faster here
-    // imp.expect(shelloTime).to.be.above(shelljsCmdTime * 1.0)
+        imp.expect(shelljsExecTime).to.be.above(shelloTime * 9)
+        imp.expect(shelloTime).to.be.above(shelljsCmdTime * 4)
+      })
+    })
+
+
+    describe('which', function() {
+
+      before(function() {
+        var which1 = shello('where which', {stdio: 'pipe', encoding: 'utf-8'})
+        var which2 = shello('which which', {stdio: 'pipe', encoding: 'utf-8'})
+        if (!which1.ok && !which2.ok) {
+          this.skip()
+        }
+      })
+
+      it('which', function() {
+
+        start = now()
+        cmdObj = shelljs.exec('which echo', {silent: true})
+        end = now()
+        imp.expect(cmdObj.code).to.equal(0)
+        shelljsExecTime = end - start
+        console.log('shelljs.exec time -----> ', shelljsExecTime.toFixed(2))
+
+        start = now()
+        cmdObj = shello('which echo', {stdio: 'pipe', encoding: 'utf-8'})
+        end = now()
+        imp.expect(cmdObj.code).to.equal(0)
+        shelloTime = end - start
+        console.log('shello time -----------> ', shelloTime.toFixed(2))
+
+        start = now()
+        cmdObj = shelljs.which('echo')
+        end = now()
+        imp.expect(cmdObj).to.not.be.null
+        imp.expect(cmdObj.code).to.equal(0)
+        shelljsCmdTime = end - start
+        console.log('shelljs.which time ----> ', shelljsCmdTime.toFixed(2))
+
+        imp.expect(shelljsExecTime).to.be.above(shelloTime * 1.8)
+        imp.expect(shelloTime).to.be.above(shelljsCmdTime * 1.2)
+      })
+    })
+
+    describe('cat', function() {
+
+      before(function() {
+        var cat1 = shello('where cat', {stdio: 'pipe', encoding: 'utf-8'})
+        var cat2 = shello('which cat', {stdio: 'pipe', encoding: 'utf-8'})
+        if (!cat1.ok && !cat2.ok) {
+          this.skip()
+        }
+      })
+
+      it('cat', function() {
+
+        var file1 = __dirname + '/file1'
+        var file2 = __dirname + '/file2'
+
+        start = now()
+        cmdObj = shelljs.exec('cat ' + file1 + ' ' + file2, {silent: true})
+        end = now()
+        imp.expect(cmdObj.stdout).to.equal('hello\nworld\n')
+        shelljsExecTime = end - start
+        console.log('shelljs.exec time -----> ', shelljsExecTime.toFixed(2))
+
+        start = now()
+        cmdObj = shello('cat ' + file1 + ' ' + file2, {stdio: 'pipe', encoding: 'utf-8'})
+        end = now()
+        imp.expect(cmdObj.stdout).to.equal('hello\nworld\n')
+        shelloTime = end - start
+        console.log('shello time -----------> ', shelloTime.toFixed(2))
+
+        start = now()
+        cmdObj = shelljs.cat(file1, file2)
+        end = now()
+        imp.expect(cmdObj.stdout).to.equal('hello\nworld\n')
+        shelljsCmdTime = end - start
+        console.log('shelljs.cat time ------> ', shelljsCmdTime.toFixed(2))
+
+        imp.expect(shelljsExecTime).to.be.above(shelloTime * 3.5)
+        // shello sometimes faster here
+        // imp.expect(shelloTime).to.be.above(shelljsCmdTime * 1.0)
+      })
+    })
   })
 })
